58fb241872126eeff5845c7a9a95ffd2c2f768c2
Waleed Asad <waleed@tixsee.com>
2017-04-25 12:57:44 +0500

    Add files via upload

---
+++ app/src/main/java/com/glide/SpacePhotoActivity.java
@@ -0,0 +1,38 @@
+package com.glide;
+
+/**
+ * Created by TixseeUser on 4/25/2017.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+public class SpacePhotoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_SPACE_PHOTO = "SpacePhotoActivity.SPACE_PHOTO";
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        SpacePhoto spacePhoto = getIntent().getParcelableExtra(EXTRA_SPACE_PHOTO);
+
+        Glide.with(this)
+                .load(spacePhoto.getUrl())
+                .asBitmap()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
+    }
+}
